a463e0cb4ead4ee301529fca5f84c5c31388a2d0
Sonu <pawan.sharma0702@gmail.com>
2022-05-09 19:47:55 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class Sum
+{
+public static void main(String [] args)
+{
+int a=10;
+int b=20;
+int c=a+b;
+int d=a*b;
+
+System.out.println("Sum of A and B"+c);
+System.out.println("Multiplication of A and B"+d);
+}
+}
