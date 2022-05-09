0e0a341fa029b3a5eaefaf5eef01e12dd611e6d8
Sonu <pawan.sharma0702@gmail.com>
2022-05-09 20:04:20 +0530

    all file are commited

--- Sum.java
+++ Sum.java
@@ -6,8 +6,10 @@ int a=10;
 int b=20;
 int c=a+b;
 int d=a*b;
+String name="PAWAN SHARMA";
 
 System.out.println("Sum of A and B"+c);
 System.out.println("Multiplication of A and B"+d);
+System.out.println("My name is ==> "+name)
 }
 }
